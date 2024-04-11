cfab90a6fc500f9e68e36c73dea676e029771371
sabr <syrym.sabyrzhan@gmail.com>
2024-04-11 14:08:37 +0500

    Feat: get specific currency done

--- src/main/java/com/example/exchange/controllers/GetSpecificCurrency.java
+++ src/main/java/com/example/exchange/controllers/GetSpecificCurrency.java
@@ -1,6 +1,10 @@
 package com.example.exchange.controllers;
 
+import com.example.exchange.dao.CurrenciesDAO;
+import com.example.exchange.entity.Currency;
 import com.example.exchange.models.DBActions;
+import com.example.exchange.services.CurrencyService;
+import com.example.exchange.util.RespondUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,14 +14,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet ("/currency/*")
 public class GetSpecificCurrency extends HttpServlet {
-    DBActions actions = new DBActions();
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        actions.getSpecificCurrency(req, resp);
-    }
+    CurrencyService currencyService = new CurrencyService();
+    RespondUtil respondUtil = new RespondUtil();
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getRequestURI();
+        String[] urls = path.split("/");
+        if(urls[urls.length - 1].length() != 3 || !urls[urls.length - 2].equals("currency")){
+            resp.setStatus(400);
+            respondUtil.showError(resp, "Wrong url");
+        } else {
+            String code = urls[urls.length - 1];
+            Currency currency = currencyService.findByCode(code);
+
+        }
     }
+
+
 }
--- src/main/java/com/example/exchange/services/CurrencyService.java
+++ src/main/java/com/example/exchange/services/CurrencyService.java
@@ -2,12 +2,17 @@ package com.example.exchange.services;
 
 import com.example.exchange.dao.CurrenciesDAO;
 import com.example.exchange.entity.Currency;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
 public class CurrencyService {
     CurrenciesDAO currenciesDAO = new CurrenciesDAO();
-    public final List<Currency> findAll(){
+    public List<Currency> findAll(){
         return currenciesDAO.findAll();
+    }
+
+    public Currency findByCode(String code){
+        return currenciesDAO.findByCode(code);
     }
 }
